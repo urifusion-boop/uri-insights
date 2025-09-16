ddf1755407100109017403c0c7ab1c3b3f5c5a90
LaProgrammer <justin.ebedi@uricreative.com>
2025-09-16 12:19:54 +0100

    Comment out SSL cert files reference.

--- app/main.py
+++ app/main.py
@@ -402,13 +402,13 @@ def custom_openapi():
 if __name__ == "__main__":
     import uvicorn
 
-    if Settings.ENV == "Development":
-        uvicorn.run(app, host="0.0.0.0", port=443)
-    else:
-        uvicorn.run(
-            app,
-            host="0.0.0.0",
-            port=443,
-            ssl_keyfile=settings.SSL_KEY_PATH,  # Path to the private key
-            ssl_certfile=settings.SSL_CERT_PATH,  # Path to the certificate
-        )
+    uvicorn.run(app, host="0.0.0.0", port=443)
+    # if Settings.ENV == "Development":
+    # else:
+    #     uvicorn.run(
+    #         app,
+    #         host="0.0.0.0",
+    #         port=443,
+    #         ssl_keyfile=settings.SSL_KEY_PATH,  # Path to the private key
+    #         ssl_certfile=settings.SSL_CERT_PATH,  # Path to the certificate
+    #     )
